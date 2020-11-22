11c4a122ba8ecd1ea05208727d4d0277d45a0096
Aamir Bhat <aamirbhat382@gmail.com>
2020-11-22 23:10:57 +0530

    updated

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -1,8 +1,21 @@
+const json = require("express")
+
 function cartController() {
     return {
         cart: function(req, res) {
             res.render("./customers/cart")
+        },
+        update: function(req, res) {
+            if (!req.session.cart) {
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0
+                }
+            }
         }
+
     }
+
 }
 module.exports = cartController
---
+++ public/js/clint.js
@@ -0,0 +1,6 @@
+let addToCart = document.querySelectorAll(".add-to-cart");
+addToCart.forEach((Btn) => {
+    Btn.addEventListener("click", (e) => {
+        console.log(e)
+    })
+})
