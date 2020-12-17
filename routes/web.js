3261825214bf3ad1190de5f9049c995431d66ac6
Aamir Bhat <aamirbhat382@gmail.com>
2020-12-17 12:05:18 +0530

    single page order

--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,7 @@ const authController = require("../app/http/controllers/customers/authController
 const cartController = require("../app/http/controllers/customers/cartController")
 const orderController = require('../app/http/controllers/customers/orderController')
 const adminOrderController = require('../app/http/controllers/admin/orderController')
+const adminStatusController = require('../app/http/controllers/admin/statusController')
     // Middlewares
 const guest = require("../app/http/middlewares/guest")
 const auth = require("../app/http/middlewares/auth")
@@ -27,7 +28,9 @@ function routes(app) {
         // Customers Route
     app.post("/orders", auth, orderController().store)
     app.get("/customer/orders", auth, orderController().index)
+    app.get("/customer/orders/:id", auth, orderController().show)
         // Admin Route
     app.get('/admin/orders', admin, adminOrderController().index)
+    app.post('/admin/order/status', admin, adminStatusController().index)
 }
 module.exports = routes
